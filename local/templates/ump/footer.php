73c7f720853db7bd01e5a80541b97dab90aa0ca8
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2020-05-25 21:02:22 +0300

    Added footer

--- local/templates/ump/footer.php
+++ local/templates/ump/footer.php
@@ -1,6 +1,71 @@
-<?
-if(defined("B_PROLOG_INCLUDED") && B_PROLOG_INCLUDED===true)
-{
-	require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog.php");
-}
-?>
+<footer class="footer">
+			<div class="footer__top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-2">
+							<img src="/local/templates/ump/img/logo.png" alt="АО «ЮМП»">
+						</div>
+						<div class="col-lg-3">
+							<ul class="footer__list">
+								<li class="footer__item">
+									<a href="#" class="text">О компании</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Вакансии</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Сертификаты дистрибьютора</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Поддержка и сервис</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Продукты</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Партнёрский раздел</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Политика конфиденциальности</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Контакты</a>
+								</li>								
+							</ul>
+						</div>
+						<div class="col-lg-3">
+							<ul class="footer__list">
+								<li class="footer__item">
+									<a href="#" class="text">Ударно-точечная маркировка</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Оборудование для печати</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Кабельные наконечники и инструменты</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Кабель и провод</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Промышленная безопасность</a>
+								</li>
+								<li class="footer__item">
+									<a href="#" class="text">Клеи, герметики, промышленная химия</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer__bottom">
+				<div class="container">
+					<div class="text text_xsmall footer__copyright">
+						© Все права защищены. Объединённый сайт компании АО "ЮНИТ МАРК ПРО". 2019
+					</div>
+					<div class="text text_xsmall text_lightgrey">
+						«АО «ЮМП» использует файлы «Сookie», с целью персонализации сервисов и повышения удобства пользования веб-сайтом. «Cookie» представляют собой небольшие файлы, содержащие информацию о предыдущих посещениях веб-сайта. Если вы не хотите использовать файлы «Сookie», измените настройки Вашего браузера.
+					</div>
+				</div>
+			</div>
+		</footer>
